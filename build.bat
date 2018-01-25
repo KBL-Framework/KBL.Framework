f58ede157491359fb8a7bf7f6be026a18b5bd186
kubalunacek <jakub.lunacek@outlook.com>
2018-01-25 12:46:31 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,6 @@
 @ECHO OFF
 SETLOCAL
-SET VERSION=%1
-SET NUGET=.\bin\nuget.exe
 
 start dotnet restore KBL.Framework && dotnet build KBL.Framework
 
 xcopy ".\KBL.Framework\KBL.Framework.Base\bin\Release\netcoreapp2.0\*.dll" ".\nuget\lib\netcoreapp2.0\" /Y
-
-FOR /r %%f IN (*.nuspec) DO (
-  echo "packing..."
-  %NUGET% pack %%f -Version %VERSION%
-)
