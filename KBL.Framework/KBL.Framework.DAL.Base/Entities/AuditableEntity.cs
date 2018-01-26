d7a7c774cd2e4ebd7c0d3b327e50b62595724463
Jakub Luňáček <jakub.lunacek@cez.cz>
2018-01-26 07:25:39 +0100

    Added AuditableEntity in DAL BASE.  This Entity has fields for IDs of user, which manipulate it.

---
+++ KBL.Framework/KBL.Framework.DAL.Base/Entities/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KBL.Framework.DAL.Base.Entities
+{
+    public abstract class AuditableEntity : BaseEntity
+    {
+        #region Properties
+        public long CreatedBy { get; set; }
+        public long? ModifiedBy { get; set; }
+        public long? DeletedBy { get; set; }
+        #endregion
+    }
+}
