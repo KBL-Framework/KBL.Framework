b013af9211e7c69760353b657cd065dd3b24f10e
Jakub Luňáček <jakub.lunacek@cez.cz>
2018-08-14 17:56:19 +0200

    Solve issue #19.

---
+++ KBL.Framework/KBL.Framework.BAL.Base/Entities/AuditableDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBL.Framework.BAL.Base.Entities
+{
+    public class AuditableDto : BaseTimestampDto
+    {
+        #region Properties       
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public string DeletedBy { get; set; }
+        #endregion
+    }
+}
