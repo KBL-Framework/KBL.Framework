f549da18e7365956790cb0001e8ef103fcf25143
Jakub Luňáček <jakub.lunacek@cez.cz>
2018-12-04 09:27:22 +0100

    Generate procedures extensions example.

--- KBL.Framework/KBL.Framework.DAL.Interfaces/Repositories/ICrudRepository.cs
+++ KBL.Framework/KBL.Framework.DAL.Interfaces/Repositories/ICrudRepository.cs
@@ -3,6 +3,8 @@ using KBL.Framework.DAL.Interfaces.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace KBL.Framework.DAL.Interfaces.Repositories
 {
@@ -11,5 +13,11 @@ namespace KBL.Framework.DAL.Interfaces.Repositories
         ICrudResult<T> Add(T entity);
         ICrudResult<T> Delete(T entity);
         ICrudResult<T> Update(T entity);
+        Task<ICrudResult<T>> AddAsync(T entity);
+        Task<ICrudResult<T>> DeleteAsync(T entity);
+        Task<ICrudResult<T>> UpdateAsync(T entity);
+        //Task<ICrudResult<T>> AddAsync(T entity, CancellationToken cancellationToken);
+        //Task<ICrudResult<T>> DeleteAsync(T entity, CancellationToken cancellationToken);
+        //Task<ICrudResult<T>> UpdateAsync(T entity, CancellationToken cancellationToken);
     }
 }
