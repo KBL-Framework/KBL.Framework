1062c67b4152b7bc0f0fc69d36bd2658bb4aa55c
Jakub Luňáček <jakub.lunacek@outlook.com>
2019-01-18 12:30:19 +0100

    Replaced DateTime by DateTimeOffset

--- KBL.Framework/KBL.Framework.DAL.Interfaces/Entities/IEntity.cs
+++ KBL.Framework/KBL.Framework.DAL.Interfaces/Entities/IEntity.cs
@@ -7,8 +7,8 @@ namespace KBL.Framework.DAL.Interfaces.Entities
     public interface IEntity
     {
         long ID { get; set; }
-        DateTime CreatedDateTime { get; set; }
-        DateTime? ModifiedDateTime { get; set; }
-        DateTime? DeletedDateTime { get; set; }
+        DateTimeOffset CreatedDateTime { get; set; }
+        DateTimeOffset? ModifiedDateTime { get; set; }
+        DateTimeOffset? DeletedDateTime { get; set; }
     }
 }
