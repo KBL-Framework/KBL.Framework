ec5188da85f9ddb8c984dba9e3bbeebb96f72d5f
vEnCa-X <vencax@gmail.com>
2021-05-29 17:36:12 +0200

    Travis build for net5

--- build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SETLOCAL
-
-start dotnet restore KBL.Framework && dotnet build KBL.Framework
-
-xcopy ".\KBL.Framework\KBL.Framework.BAL.Base\bin\Release\netcoreapp2.0\*.dll" ".\nuget\lib\netcoreapp2.0\" /Y
